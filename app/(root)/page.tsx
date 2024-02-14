e1c184898d622a3f17dad5fd95ad4c38e1d0bfff
thomasdinh2k <work.dnstung@gmail.com>
2024-02-14 22:06:53 +0700

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return <div className="p-5">
+
+		<Button size="lg" variant="outline"> Click me please! </Button>
+	
+	</div>;
+}
